d20699439a263e4ddcdae9a5fe030c8b3672032a
doud21 <doud@berkeley.edu>
2022-03-15 20:00:04 -0700

    3/15 meeting work sesh

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import homePhoto from './images/homePhoto.png'
+import glow from './images/glow.svg'
+import homeLogo from './images/homeLogo.svg'
+
+function NavItem(props) {
+  return (
+    <h1 className="navbar-item">{props.name}</h1>
+  );
+}
+// home button 
+function HomeButton() {
+  return (
+    <img src = {homeLogo} className="logo"/>
+  )
+}
+
+function Nav() {
+  return (
+    <div className="nav">
+      <HomeButton></HomeButton>
+      <div className="navbar-links">
+        <NavItem name={"About"}></NavItem>
+        <NavItem name={"Championship"}></NavItem>
+        <NavItem name={"Competitive"}></NavItem>
+        <NavItem name={"Shop"}></NavItem>
+        <NavItem name={"Discord"}></NavItem> // will turn this into an image in a bit
+      </div>
+    </div>
+  )
+}
+
+function Home() {
+  return(
+    <div>
+      <img src = {homePhoto}/>
+      <img src = {glow}/>
+      <h2>UC BERKELEY'S OFFICIAL RIOT - SPONSORED CLUB</h2>
+
+      <p>
+        Berkeley Legends strives to ensure that UC Berkeley students 
+        have a place to create relationships with other Riot Games 
+        players. Come join us at our viewing parties, socials, LAN 
+        events, tournaments, and more.
+      </p>
+    </div>
+    
+  )
+}
+
+function button() {
+  
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <Nav></Nav>
+      <Home></Home>
     </div>
   );
 }
+
 
 export default App;
