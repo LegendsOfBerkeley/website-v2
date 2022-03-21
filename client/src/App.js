de9dcfca5d686d9f2504ee4fa36a9079aa9c2710
Jared Basilio <jaredbasilio@berkeley.edu>
2022-03-21 02:18:40 -0700

    navbar and landing page progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import homePhoto from './images/homePhoto.png'
+import jett from './images/jett.png'
 import glow from './images/glow.svg'
 import Nav from './navbar/Nav.js'
 
 function Home() {
   return(
-    <div>
-      <img src = {homePhoto}/>
-      <img src = {glow}/>
-      <h2>UC BERKELEY'S OFFICIAL RIOT - SPONSORED CLUB</h2>
-
-      <p>
-        Berkeley Legends strives to ensure that UC Berkeley students 
-        have a place to create relationships with other Riot Games 
-        players. Come join us at our viewing parties, socials, LAN 
-        events, tournaments, and more.
-      </p>
+    <div className="landing">
+      <img src = {homePhoto} className="team-landing-image"/>
+      <div className="right-landing">
+        <img src = {glow} className="logo-landing"/>
+        <h2>UC BERKELEY'S OFFICIAL RIOT - SPONSORED CLUB</h2>
+        <p className="landing-info">
+          Berkeley Legends strives to ensure that UC Berkeley students 
+          have a place to create relationships with other Riot Games 
+          players. Come join us at our viewing parties, socials, LAN 
+          events, tournaments, and more.
+        </p>
+      </div>
+      <img src = {jett} className="jett-landing"></img>
     </div>
     
   )
--- client/src/navbar/Nav.js
+++ client/src/navbar/Nav.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import homeLogo from '../images/homeLogo.svg';
 import './Nav.css';
+import discord from '../images/Discord-Purple.svg'
 
 function NavItem(props) {
     return (
       <h1 className="navbar-item">{props.name}</h1>
     );
-  }
+}
+
+function NavItemIcon(props) {
+    return (
+        <img src = {props.icon} className="navbar-item"/> //maybe button class, i forgot which class we were going to use -Jared
+    )
+}
+
 // home button 
 function HomeButton() {
     return (
@@ -23,7 +31,7 @@ function Nav() {
             <NavItem name={"Championship"}></NavItem>
             <NavItem name={"Competitive"}></NavItem>
             <NavItem name={"Shop"}></NavItem>
-            <NavItem name={"Discord"}></NavItem>
+            <NavItemIcon icon={discord}></NavItemIcon>
         </div>
     </div>
     )
