d84390f7091e2704d796e096859a6320ffe05953
Jared Basilio <jaredbasilio@berkeley.edu>
2022-03-26 14:32:52 -0700

    landing page tweaks and centering

--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,29 @@ import homePhoto from './images/homePhoto.png'
 import jett from './images/jett.png'
 import glow from './images/glow.svg'
 import games from './images/games.png'
+import events from './images/button_events.png'
 import sponsors from './images/sponsors.png'
 import Nav from './navbar/Nav.js'
 
 function Home() {
   return(
-    <div className="landing">
-      <img src = {homePhoto} className="team-landing-image"/>
-      <div className="right-landing">
-        <img src = {glow} className="logo-landing"/>
-        <h2>UC BERKELEY'S OFFICIAL RIOT - SPONSORED CLUB</h2>
-        <p className="landing-info">
-          Berkeley Legends strives to ensure that UC Berkeley students 
-          have a place to create relationships with other Riot Games 
-          players. Come join us at our viewing parties, socials, LAN 
-          events, tournaments, and more.
-        </p>
-      </div>
-      <img src = {jett} className="jett-landing"></img>
-    </div> 
+    <div className="showcase">
+      <div className="landing-container">
+        <img src = {homePhoto} className="team-landing-image"/>
+        <div className="right-landing">
+          <img src = {glow} className="logo-landing"/>
+          <h2 className="subheader">UC BERKELEY'S OFFICIAL RIOT - SPONSORED CLUB</h2>
+          <p className="landing-info">
+            Berkeley Legends strives to ensure that UC Berkeley students 
+            have a place to create relationships with other Riot Games 
+            players. Come join us at our viewing parties, socials, LAN 
+            events, tournaments, and more.
+          </p>
+          <img src = {events}></img>
+        </div>
+        <img src = {jett} className="jett-landing"></img>
+      </div> 
+    </div>
   )
 }
 function GamesWeRepresent() {
