9480bef50fc46b654c2b523fc011c6099b5ece37
Jennifer Yang <jennifer.yang818@gmail.com>
2022-03-29 20:00:10 -0700

    worked on about page

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import About from './about/about';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <About />
   </React.StrictMode>,
   document.getElementById('root')
 );
