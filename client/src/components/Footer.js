96a936d54c431ad900dab794fd500a65c6619b53
doud21 <doud@berkeley.edu>
2022-04-19 19:58:34 -0700

    footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,4 +1,9 @@
 import '../styles/Footer.css';
+import Discord from '../images/footer/discord.svg'
+import Twitch from '../images/footer/twitch.svg'
+import Facebook from '../images/footer/facebook.svg'
+import Instagram from '../images/footer/instagram.svg'
+import Twitter from '../images/footer/twitter.svg'
 
 function Footer() {
   return (
@@ -9,7 +14,11 @@ function Footer() {
             <br></br>
             <br></br>
             <a>Cal Women in Gaming</a>
+            <br></br>
+            <br></br>
             <a>Smash at Berkeley</a>
+            <br></br>
+            <br></br>
             <a>Gaming at Berkeley</a>
         </div>
         <div className="contact">
@@ -18,7 +27,14 @@ function Footer() {
             <p>Message us on our <a href = "https://discord.com/invite/BQvHDnB">Discord</a></p>
         </div>
         <div className="find">
-            hi
+            <h4>FIND US ON</h4>
+            <div className="icons">
+                <img src={Discord}/>
+                <img src={Twitch}/>
+                <img src={Facebook}/>
+                <img src={Instagram}/>
+                <img src={Twitter}/>
+            </div>
         </div>
     </div>
   );
