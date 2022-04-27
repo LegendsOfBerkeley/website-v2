f807b11ef166ae5d00732854b947c18c4dc1d50a
Cynthia Shi <cynthiashi@Cynthias-M1-MacBook-Pro.local>
2022-04-26 19:56:51 -0700

    Competitive card changes

--- client/src/pages/Competitive.js
+++ client/src/pages/Competitive.js
@@ -3,6 +3,7 @@ import './Competitive.css'
 import Nav from '../components/navbar/Nav'
 import ThinLegendsButton from '../components/ThinLegendsButton'
 import {Link} from "react-router-dom";
+import amy_pic from '../images/amy-profile-pic.png';
 
 function Header() {
     return (
@@ -27,6 +28,16 @@ function League() {
     return (
         <div>
             <h4 className='header'>LEAGUE OF LEGENDS</h4>
+            <div id="cards">
+                <Card icon={amy_pic} name={"AMY JIANG "} role={"| COACH"} major={"MECHANICAL ENGINEERING + EECS"} year={"CLASS OF 2022"} bio={"Favorite Riot Character: answer"}></Card>
+                <div className='card-grid-container'>
+                    <Card icon={amy_pic} name={"AMY JIANG "} role={"| COACH"} major={"MECHANICAL ENGINEERING + EECS"} year={"CLASS OF 2022"} bio={"Favorite Riot Character: answer"}></Card>
+                    <Card icon={amy_pic} name={"AMY JIANG "} role={"| COACH"} major={"MECHANICAL ENGINEERING + EECS"} year={"CLASS OF 2022"} bio={"Favorite Riot Character: answer"}></Card>
+                    <Card icon={amy_pic} name={"AMY JIANG "} role={"| COACH"} major={"MECHANICAL ENGINEERING + EECS"} year={"CLASS OF 2022"} bio={"Favorite Riot Character: answer"}></Card>
+                    <Card icon={amy_pic} name={"AMY JIANG "} role={"| COACH"} major={"MECHANICAL ENGINEERING + EECS"} year={"CLASS OF 2022"} bio={"Favorite Riot Character: answer"}></Card>
+                    <Card icon={amy_pic} name={"AMY JIANG "} role={"| COACH"} major={"MECHANICAL ENGINEERING + EECS"} year={"CLASS OF 2022"} bio={"Favorite Riot Character: answer"}></Card>
+                </div>
+            </div>
         </div>
     )
 }
@@ -43,19 +54,32 @@ function Valorant() {
     return (
         <div>
             <h4 className='header'>VALORANT</h4>
+            <div id="cards">
+                <Card icon={amy_pic} name={"AMY JIANG "} role={"| COACH"} major={"MECHANICAL ENGINEERING + EECS"} year={"CLASS OF 2022"} bio={"Favorite Riot Character: answer"}></Card>
+                <div className='card-grid-container'>
+                    <Card icon={amy_pic} name={"AMY JIANG "} role={"| COACH"} major={"MECHANICAL ENGINEERING + EECS"} year={"CLASS OF 2022"} bio={"Favorite Riot Character: answer"}></Card>
+                    <Card icon={amy_pic} name={"AMY JIANG "} role={"| COACH"} major={"MECHANICAL ENGINEERING + EECS"} year={"CLASS OF 2022"} bio={"Favorite Riot Character: answer"}></Card>
+                    <Card icon={amy_pic} name={"AMY JIANG "} role={"| COACH"} major={"MECHANICAL ENGINEERING + EECS"} year={"CLASS OF 2022"} bio={"Favorite Riot Character: answer"}></Card>
+                    <Card icon={amy_pic} name={"AMY JIANG "} role={"| COACH"} major={"MECHANICAL ENGINEERING + EECS"} year={"CLASS OF 2022"} bio={"Favorite Riot Character: answer"}></Card>
+                    <Card icon={amy_pic} name={"AMY JIANG "} role={"| COACH"} major={"MECHANICAL ENGINEERING + EECS"} year={"CLASS OF 2022"} bio={"Favorite Riot Character: answer"}></Card>
+                </div>
+            </div>
         </div>
     )
 }
 function Card(props) {
     return (
-      <div>
-        <div id="picture">
-          <img src={props.icon}></img>
+      <div className='card-container'>
+        <div classname='picture'><img src={props.icon}></img></div>
+        <div classname='info'>
+            <div classname='id-header'>
+                <div className='name'>{props.name}</div>
+                <div className='role'>{props.role}</div>
+            </div>
+            <div className='major'>{props.major}</div>
+            <div classname='year'>{props.year}</div>
+            <p classname='bio'>{props.bio}</p>
         </div>
-        <div id="name">{props.name}</div>
-        <div id="name">{props.alias}</div>
-        <div id="position">{props.position}</div>
-        <p id="bio">{props.bio}</p>
       </div>
     )
   }
