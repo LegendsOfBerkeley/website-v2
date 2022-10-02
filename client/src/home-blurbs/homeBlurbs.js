0d671cbc8acb88fa972ecec3ba90cb5439e634c8
Amy Jiang <p.amyjiang@berkeley.edu>
2022-10-02 13:44:48 -0700

    added *some button functionality

--- client/src/home-blurbs/homeBlurbs.js
+++ client/src/home-blurbs/homeBlurbs.js
@@ -31,9 +31,7 @@ function RightBlurb(props) {
                     <p className="blurb-title-text">{props.title}</p>
                 </div>
                 <p className="blurb-paragraph">{props.blurb}</p>
-                <Link to={props.link}>
-                    <ThinLegendsButton title={props.button}></ThinLegendsButton>
-                </Link>
+                <ThinLegendsButton title={props.button}></ThinLegendsButton>
             </div>
         </div>
     )
@@ -62,7 +60,7 @@ function AllBlurbs(props) {
         <div>
             <RightBlurb img={photo_community} icon={users_group_icon} title={"Community"} blurb={"Our community loves discussing the latest competitive esports games, character reworks, map changes, and finding new friends to duo queue or play inhouses with. We welcome any students interested in League, Teamfight Tactics, Legends of Runeterra, or Valorant, regardless of level or rank."} button={"JOIN OUR COMMUNITY"}></RightBlurb>
             <LeftBlurb img={photo_events} icon={calendar_check_icon} title={"Events"} blurb={"From LAN parties to tournaments to board game nights, Berkeley Legends hosts a variety of events open to all Berkeley students! Keep an eye on our event calendar for upcoming events, and check out our photo albums for memories from the past ones."} link={"/about"} button={"VIEW UPCOMING EVENTS"} ></LeftBlurb>
-            <RightBlurb img={photo_championship} icon={championship_icon} title={"Championship"} blurb={"For those who enjoy the company at Berkeley Legends and want to see more, we offer an optional paid Champions program granting bonus perks such as access to family system, Champion-exclusive socials, free tournament admission, custom discord emotes, and more!"} link={"/championship"} button={"BECOME A CHAMPION"} ></RightBlurb>
+            <RightBlurb img={photo_championship} icon={championship_icon} title={"Championship"} blurb={"For those who enjoy the company at Berkeley Legends and want to see more, we offer an optional paid Champions program granting bonus perks such as access to family system, Champion-exclusive socials, free tournament admission, custom discord emotes, and more!"} button={"BECOME A CHAMPION"} ></RightBlurb>
             <LeftBlurb img={photo_competitive} icon={team_icon} title={"Competitive Teams"} blurb={"We are proud to host Division 1 and 2 collegiate esports teams in League of Legends and Valorant, representing UC Berkeley at tournaments across the country. Find out more about our teams and make sure to cheer on the Golden Bears this season!"} link={"/competitive"}  button={"LEARN ABOUT COMPETITIVE"} ></LeftBlurb>
         </div>
     )
