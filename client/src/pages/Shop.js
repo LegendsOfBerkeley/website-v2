269057642409d9f400337b9df8de5328c5d35f5b
Amy Jiang <p.amyjiang@berkeley.edu>
2022-11-21 23:11:41 -0800

    updated shop page

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -6,7 +6,11 @@ import {Link} from "react-router-dom";
 import placeholder from '../images/shop/placeholder.png'
 import championMedal from '../images/icons/champion-medal.png'
 import {useState} from 'react'; 
-import placeholder2 from '../images/shop/placeholder2.png'
+import charmjuice from '../images/shop/charmjuice.jpg'
+import meepjuice from '../images/shop/meepjuice.jpg'
+import souljuice from '../images/shop/souljuice.jpg'
+import meeppin from '../images/shop/meeppin.jpg'
+import poropin from '../images/shop/poropin.jpg'
 import placeholder3 from '../images/shop/placeholder3.png'
 import Footer from '../components/Footer'
 
@@ -14,15 +18,17 @@ import Footer from '../components/Footer'
 function Header() {
   return (
       <div>
-          <h3 className="shop-header">SHOP</h3>
+          <h3 className="shop-header">Merchandise Gallery</h3>
           <p className="shop-info">
-          <img className="champ-icon" src={championMedal}/> Verified Champions receive a $5 off discount on all merchandise options!
+          {/* <img className="champ-icon" src={championMedal}/>  */}
+          View our selection of Berkeley Legends merchandise, drawn and created by artists in our community!  
           </p>
-          <Link to="/championship">
+          <br></br><br></br>
+          {/* <Link to="/championship">
             <div className="shop-champ-button">
               <ThinLegendsButton title="BECOME A CHAMPION"></ThinLegendsButton>
             </div>
-          </Link>
+          </Link> */}
       </div>
   )
 }
@@ -48,19 +54,29 @@ function ShopApparel(props) {
   return (
     <div className="shop-row">
       <ShopItem image={placeholder} product={"Ruined King x Berkeley Legends Crewneck"} regular={"$35.00"} champ={"$30.00"}></ShopItem>
-      <ShopItem image={placeholder} product={"Ruined King x Berkeley Legends Crewneck"} regular={"$35.00"} champ={"$30.00"}></ShopItem>
-      <ShopItem image={placeholder} product={"Ruined King x Berkeley Legends Crewneck"} regular={"$35.00"} champ={"$30.00"}></ShopItem>
+      <ShopItem image={placeholder} product={"Fall 2022 Hoodie"} regular={"$35.00"} champ={"$30.00"}></ShopItem>
+      {/* <ShopItem image={placeholder} product={"Ruined King x Berkeley Legends Crewneck"} regular={"$35.00"} champ={"$30.00"}></ShopItem> */}
 
     </div>
   )
 }
 
-function ShopAccessories(props) {
+function ShopAccessories1(props) {
   return (
     <div className="shop-row">
-      <ShopItem image={placeholder2} product={"Ruined King x Berkeley Legends Accessory"} regular={"$35.00"} champ={"$30.00"}></ShopItem>
-      <ShopItem image={placeholder2} product={"Ruined King x Berkeley Legends Accessory"} regular={"$35.00"} champ={"$30.00"}></ShopItem>
-      <ShopItem image={placeholder2} product={"Ruined King x Berkeley Legends Accessory"} regular={"$35.00"} champ={"$30.00"}></ShopItem>
+      <ShopItem image={charmjuice} product={"Charm Juice Sticker"} regular={"$35.00"} champ={"$30.00"}></ShopItem>
+      <ShopItem image={meepjuice} product={"Meep Juice Sticker"} regular={"$35.00"} champ={"$30.00"}></ShopItem>
+      <ShopItem image={souljuice} product={"Soul Juice Sticker"} regular={"$35.00"} champ={"$30.00"}></ShopItem>
+    </div>
+  )
+}
+
+//temp hardcoded implementation for multiple rows. todo: implement resizable grids.
+function ShopAccessories2(props) {
+  return (
+    <div className="shop-row">
+      <ShopItem image={meeppin} product={"Meep Boba Pin"} regular={"$35.00"} champ={"$30.00"}></ShopItem>
+      <ShopItem image={poropin} product={"Poro Boba Pin"} regular={"$35.00"} champ={"$30.00"}></ShopItem>
     </div>
   )
 }
@@ -77,6 +93,8 @@ function ShopPrints(props) {
 
 function Shop() {
   let [category, setCategory] = useState("apparel"); 
+  let appClassName = 'category-button';
+  let accClassName = 'category-button';
 
   function setApparel() {
     setCategory("apparel"); 
@@ -90,6 +108,15 @@ function Shop() {
     setCategory("prints"); 
   }
 
+  if (category == "apparel") {
+    appClassName += ' category-button-active';
+  }
+
+  if (category == "accessories") {
+    accClassName += ' category-button-active';
+  }
+
+
   return (
     <div className="body">
       <Nav/>
@@ -97,17 +124,23 @@ function Shop() {
       <div className="category">
       {/* will try to make the buttons underline with the states */}
         <div className="category-buttons-div">
-          <button className="category-button" onClick={() => setApparel()}>Apparel</button>
-          <button className="category-button" onClick={() => setAccessories()}>Accessories</button>
-          <button className="category-button" onClick={() => setPrints()}>Prints</button>
+          <button className={appClassName} onClick={() => setApparel()}>Apparel</button>
+          <button className={accClassName} onClick={() => setAccessories()}>Accessories</button>
+          {/* <button className="category-button" onClick={() => setPrints()}>Prints</button> */}
         </div>
         {category === "apparel" && <ShopApparel/>}
-        {category === "accessories" && <ShopAccessories/>}
-        {category === "prints" && <ShopPrints/>}
+        {category === "accessories" && <><ShopAccessories1 /><ShopAccessories2 /></>}
+        {/* {category === "prints" && <ShopPrints/>} */}
       </div>
-      <div className="order-form">
-        <ThinLegendsButton title="GO TO ORDER FORM"></ThinLegendsButton>
-      </div>
+      <p className='shop-info'>
+        Reach out to us on Discord to order and for the most updated stock and pricing.
+      </p>
+      <a href='https://tinyurl.com/BLDisco' target="_blank" rel="noopener noreferrer">
+        <div className="order-form">
+          <ThinLegendsButton title="DISCORD"></ThinLegendsButton>
+        </div>
+      </a>
+      
       <Footer />
     </div>
   )
